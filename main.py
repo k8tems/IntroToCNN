4b3dfdf8293c2e4ba815aa6bd1c0a82b9057c9c7
k8tems <k8tems@live.com>
2018-09-12 00:38:14 +0900

    DRY patch coordinate computation

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import numpy as np
 
 
+def get_patch_coordinates(A_shape, patch_size):
+    return [(col, row)
+            for col in range(A.shape[1] - patch_size[1] + 1)
+            for row in range(A.shape[0] - patch_size[0] + 1)]
+
+
 def im2col(A, patch_size):
     num_patches = 6  # how do I compute this
     B = np.ndarray(shape=(num_patches, patch_size[0]*patch_size[1]))
 
     i = 0
-    for col in range(A.shape[1] - patch_size[1] + 1):
-        for row in range(A.shape[0] - patch_size[0] + 1):
-            B[i] = A[row:row+patch_size[0], col:col+patch_size[1]].ravel(order='F')
-            i += 1
+    for col, row in get_patch_coordinates(A.shape, patch_size):
+        B[i] = A[row:row+patch_size[0], col:col+patch_size[1]].ravel(order='F')
+        i += 1
 
     return B.transpose()
 
@@ -32,4 +37,8 @@ if __name__ == '__main__':
         [5, 8, 6, 9, 1, 1],
     ]
 
-    assert(np.array_equal(expected, im2col(A, [2, 2])))
+    B = im2col(A, [2, 2])
+
+    print(B)
+
+    assert(np.array_equal(expected, B))
