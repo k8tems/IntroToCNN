22715e4d9d6ba0b81a566ae6f3040c2f4d71d761
k8tems <k8tems@live.com>
2018-09-12 01:53:36 +0900

    Convert comments to docstrings

--- main.py
+++ main.py
@@ -2,24 +2,24 @@ import numpy as np
 
 
 def reorder(x, num_rows):
-    # Inverse of `flatten
+    """Inverse of `flatten"""
     return x.reshape((-1, num_rows)).transpose()
 
 
 def flatten(x):
-    # `The input and kernel is flattened in a "column first` manner
+    """`The input and kernel is flattened in a "column first` manner"""
     return x.ravel(order='F')
 
 
 def get_patch_crds(input_shape, patch_size):
-    # Highly important for outer loop to iterate over rows and inner loop to iterate over cols
+    """Highly important for outer loop to iterate over rows and inner loop to iterate over cols"""
     return [(col, row)
             for col in range(input_shape[1] - patch_size[1] + 1)
             for row in range(input_shape[0] - patch_size[0] + 1)]
 
 
 def im2col(x, patch_size):
-    # Apply MATLAB style `im2col` expansion to `A`
+    """Apply MATLAB style `im2col` expansion to `A`"""
     patch_crds = get_patch_crds(x.shape, patch_size)
     B = np.ndarray(shape=(patch_size[0] * patch_size[1], len(patch_crds)))
 
